c093ee1463fb80e83cdbf379f1e9ee49b4f90ccf
kwdaaa <kwd.aaa.a@icloud.com>
2022-01-23 16:44:48 +0900

    タイトル： ４文字以上２０文字以内のチェック 大文字に変換する
    詳細：

---
+++ username.py
@@ -0,0 +1,36 @@
+"""
+データ型の宣言： username
+    属性：
+        実際のユーザー名
+    ルール；
+        ユーザー名は4文字以上20文字以内ある
+    できること：
+        ユーザー名を大文字に変換する
+"""
+
+
+# 「UserName」というクラスを宣言
+#  「self.name」はそれぞれのname
+class UserName:
+    # 右のnameは渡された変数（BobやらTomやら）
+    # 「__init__」は引数も渡してあげないとダメ。
+    def __init__(self, name):
+        # nameのチェック（4文字以上20文字以内を達成していない場合にエラー出す。）
+        # ※len()は文字列であれば文字数のカウントも可能
+        # 4 <= len(name) <= 20でもOK。
+        if not (4 <= len(name) and len(name) <= 20):
+            # エラーを出力する。
+            raise ValueError(f'{name}はルール違反です')
+        self.name = name
+
+    def upper_name(self):
+        # .upper()で大文字にする。
+        return self.name.upper()
+
+
+# 実体化　上の設計図に
+bob = UserName(name='Bob Smith')
+
+print(bob.name)
+# bobに「upper_name()」関数を使って大文字にする。
+print(bob.upper_name())
